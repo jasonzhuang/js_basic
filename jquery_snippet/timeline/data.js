6ecf0d9ab8f6ddecb496af3b9ef2fadef982689b
jasonzhuang <bluezyg@gmail.com>
2014-05-29 23:21:25 +0800

    dynamic add month list

--- jquery_snippet/timeline/data.js
+++ jquery_snippet/timeline/data.js
@@ -5,3 +5,41 @@
  * Time: 下午10:35
  * To change this template use File | Settings | File Templates.
  */
+var sourceData = [
+    {
+       date: '2014-02-26',
+       like: 25602,
+       intro: '14年2月第一篇',
+       media: '<img src="">'
+    },
+    {
+        date: '2014-02-20',
+        like: 25602,
+        intro: '14年2月第一篇',
+        media: '<img src="">'
+    },
+    {
+        date: '2014-03-26',
+        like: 25602,
+        intro: '14年3月第二篇',
+        media: '<img src="">'
+    },
+    {
+        date: '2013-02-26',
+        like: 25602,
+        intro: '13年2月第一篇',
+        media: '<img src="">'
+    },
+    {
+        date: '2013-03-26',
+        like: 25602,
+        intro: '13年3月第一篇',
+        media: '<img src="">'
+    },
+    {
+        date: '2013-03-28',
+        like: 25602,
+        intro: '13年3月第二篇',
+        media: '<img src="">'
+    }
+]
--- jquery_snippet/timeline/index.js
+++ jquery_snippet/timeline/index.js
@@ -1,10 +1,31 @@
+// 格式化数据
 /**
- * Created with JetBrains WebStorm.
- * User: yougen
- * Date: 14-5-26
- * Time: 下午10:35
- * To change this template use File | Settings | File Templates.
+ *  {
+    *      2014: {
+    *          2: [
+    *              {
+    *                  date: '2014-02-25',
+    *                  year: '2014',
+    *                  month: '2'
+    *                  ...
+    *              },
+    *              // ... 同个月内若干日志
+    *          ],
+    *          3:[]
+    *          // ... 一年内若干月份
+    *      }，
+    *      2013：{}
+    *      // ...
+    *  }
+ *
  */
+(function(){
+     var data = serialData(sourceData);
+    generateTime(data);
+    generateContent(data);
+})();
+
+
 function serialData(data){
     var list = {};
     $.each(data, function(index, val){
@@ -13,7 +34,7 @@ function serialData(data){
        var month = date.getMonth() + 1;
 
        list[year] || (list[year] = {});
-       list[year][month] || (list[year][month] = {});
+       list[year][month] || (list[year][month] = []);
 
        var item = val;
        item.year = year;
@@ -21,4 +42,53 @@ function serialData(data){
        item.like_format = item.like < 10000 ? item.like : (item.like / 10000)
        list[year][month].push(item);
     });
+    return list;
+}
+
+// 时序菜单HTML生成
+function generateTime(list){
+    var html_scrubber_list = [],
+        tpl_year = $("#tpl_scrubber_year").html(),
+        tpl_month = $("#tpl_scrubber_month").html();
+    $.each(list, function(year_key, year_list){
+       var html_year = tpl_year.replace(/\{year\}/g, year_key);
+       var html_month = [];
+       $.each(year_list, function(month_key, month){
+           html_month.unshift(tpl_month.replace(/\{month\}/g, month_key));
+       });
+       html_year = html_year.replace(/\{list\}/g, html_month.join(''));
+       html_scrubber_list.push(html_year);
+    });
+    $("#scrubber").html(html_scrubber_list.join(''));
+}
+
+//日志列表HTML生成
+function generateContent(list){
+    var html_content_list  = [],
+        tpl_year = $("#tpl_content_year").html(),
+        tpl_month = $("#tpl_content_month").html(),
+        tpl_item = $("#tpl_content_item").html();
+
+    $.each(list, function(year_key, year_list){
+        var html_year = tpl_year.replace(/\{year\}/g, year_key);
+        var html_month = [];
+        $.each(year_list, function(month_key, month_list){
+            var html_item = []
+            $.each(month_list, function(index, item){
+                var item_html = tpl_item
+                    .replace(/\{date\}/g, item.date)
+                    .replace(/\{month\}/g, item.month)
+                    .replace(/\{intro\}/g, item.intro)
+                    .replace(/\{media\}/g, item.media)
+                    .replace(/\{position\}/g, index%2 ? "right": "left");
+                //该月所有数据
+                html_item.push(item_html);
+            });
+            html_month.unshift(tpl_month.replace(/\{month\}/g,month_key).replace(/\{list\}/g, html_item.join('')));
+        });
+        html_year = html_year.replace(/\{list\}/g, html_month.join(''));
+        html_content_list.push(html_year);
+    });
+    $("#content").html(html_content_list.join(''));
+
 }
